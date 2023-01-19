fff8310b8bcf68d0cd5b8c2dcd715ab360d7e212
ZAHID HUSSAIN KHAN <erzahidkhan101@gmail.com>
2023-01-19 14:20:51 +0530

    Render Config test

--- Controller/authController.js
+++ Controller/authController.js
@@ -71,13 +71,16 @@ exports.updateMe = catchError(async (req, res, next) => {
 
 exports.protect = catchError(async (req, res, next) => {
   let token;
-  console.log(req.headers.cookie);
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
+  if (req.headers.cookie) {
+    token = req.headers.cookie.split('=')[1];
+  }
+  console.log(req.headers.cookie.split('=')[1]);
   if (!token) {
     return next(new createError('Not Logged in'));
   }
