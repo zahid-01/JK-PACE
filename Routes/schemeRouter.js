80865284519313963cf3ee43265e06c68bd75a6f
ZAHID HUSSAIN KHAN <erzahidkhan101@gmail.com>
2023-02-02 14:26:18 +0530

    Testing Schemes

--- Routes/schemeRouter.js
+++ Routes/schemeRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const schemeRouter = express.Router();
 const schemeController = require('../Controller/schemaController');
-const authControler = require('../Controller/authController');
+// const authControler = require('../Controller/authController');
 
-schemeRouter.use(authControler.protect, authControler.verify('super-user'));
+// schemeRouter.use(authControler.protect, authControler.verify('super-user'));
 schemeRouter
   .route('/schemeUser')
   .post(schemeController.createScheme)
